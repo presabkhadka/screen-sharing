bdd2e08f0d85cbfca75bf4380b33a5bb28677855
Presab Khadka <presabkhadka30@gmail.com>
2024-09-27 16:27:08 +0545

    implemented socket.io

---
+++ client/src/components/canvas.tsx
@@ -0,0 +1,121 @@
+import React, { useRef, useEffect } from "react";
+import { io, Socket } from "socket.io-client";
+
+const CanvasComponent: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const ctx = useRef<CanvasRenderingContext2D | null>(null);
+  const socket = useRef<Socket | null>(null);
+  const isDrawing = useRef(false);
+  const lastPosition = useRef<{ x: number; y: number } | null>(null);
+
+  useEffect(() => {
+    socket.current = io("http://localhost:3000");
+
+    const canvas = canvasRef.current;
+    if (canvas) {
+      ctx.current = canvas.getContext("2d");
+      if (ctx.current) {
+        ctx.current.lineWidth = 2;
+        ctx.current.strokeStyle = "black";
+
+        socket.current.on(
+          "draw",
+          (data: { x: number; y: number; isDrawing: boolean }) => {
+            const { x, y, isDrawing } = data;
+
+            if (ctx.current) {
+              if (isDrawing) {
+                if (lastPosition.current) {
+                  ctx.current.moveTo(
+                    lastPosition.current.x,
+                    lastPosition.current.y
+                  );
+                } else {
+                  ctx.current.moveTo(x, y);
+                }
+                ctx.current.lineTo(x, y);
+                ctx.current.stroke();
+                lastPosition.current = { x, y };
+              } else {
+                ctx.current.closePath();
+                lastPosition.current = null;
+              }
+            }
+          }
+        );
+      }
+    }
+
+    return () => {
+      socket.current?.disconnect();
+    };
+  }, []);
+
+  const drawOnCanvas = (event: MouseEvent) => {
+    const rect = canvasRef.current!.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    if (ctx.current && isDrawing.current) {
+      ctx.current.lineTo(x, y);
+      ctx.current.stroke();
+      lastPosition.current = { x, y };
+      socket.current?.emit("draw", { x, y, isDrawing: true });
+    }
+  };
+
+  const startDrawing = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      isDrawing.current = true;
+      ctx.current?.beginPath();
+      const rect = canvas.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+      ctx.current?.moveTo(x, y);
+      lastPosition.current = { x, y };
+      drawOnCanvas(event.nativeEvent);
+      canvas.addEventListener("mousemove", drawOnCanvas);
+    }
+  };
+
+  const stopDrawing = () => {
+    const canvas = canvasRef.current;
+    if (canvas && ctx.current) {
+      isDrawing.current = false;
+      canvas.removeEventListener("mousemove", drawOnCanvas);
+      ctx.current.closePath();
+      socket.current?.emit("draw", { isDrawing: false });
+      lastPosition.current = null;
+    }
+  };
+
+  const resetCanvas = () => {
+    const canvas = canvasRef.current;
+    if (ctx.current && canvas) {
+      ctx.current.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <canvas
+        ref={canvasRef}
+        className="border border-gray-500 m-2 bg-white rounded-lg"
+        onMouseDown={startDrawing}
+        onMouseUp={stopDrawing}
+        onMouseLeave={stopDrawing}
+        width={500}
+        height={500}
+      />
+      <button
+        onClick={resetCanvas}
+        className="bg-custom-gradient p-2 rounded-lg hover:animate-pulse hover:text-white"
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
+
+export default CanvasComponent;
---
+++ server/server.ts
@@ -0,0 +1,57 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const port = 3000;
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+interface DrawData {
+  x: number;
+  y: number;
+  isDrawing: boolean;
+}
+
+io.on("connection", (socket) => {
+  console.log("User Connected");
+  console.log("Socket ID:", socket.id);
+
+  socket.on("register-peer-id", (peerId) => {
+    console.log("Peer ID registered:", peerId);
+    socket.broadcast.emit("peer-id", peerId);
+  });
+
+  socket.on("draw", (data: DrawData) => {
+    socket.broadcast.emit("draw", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", socket.id);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
