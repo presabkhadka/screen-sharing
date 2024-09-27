014fea121a4327580aa734240f379a830a47b458
Presab Khadka <presabkhadka30@gmail.com>
2024-09-27 16:52:40 +0545

    vite change

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
-})
+  base: "/",
+  server: {
+    host: "0.0.0.0", // Listen on all network interfaces
+    port: 5173, // Ensure you're using the correct port
+    strictPort: true, // Fail if the port is already in use
+  },
+});
