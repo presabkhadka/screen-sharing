d5651d90040a669f78490c1aeb906d04b013763f
Presab Khadka <presabkhadka30@gmail.com>
2024-09-29 14:17:02 +0545

    cors origin for all

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -18,7 +18,7 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   })
